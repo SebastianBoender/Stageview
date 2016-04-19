1e499301f2254be108e87831bd21e739cd6415cd
SebastianBoender <sebastianboender@hotmail.com>
2016-04-19 09:21:26 +0200

    Trying fix for artisan make

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -47,3 +47,8 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
