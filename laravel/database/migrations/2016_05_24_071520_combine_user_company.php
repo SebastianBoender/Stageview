6c3a385de284be633a0394a170333be5b78f8d44
SebastianBoender <sebastianboender@hotmail.com>
2016-05-24 09:32:33 +0200

    Fixed jelle's mistake #3rd time

---
+++ laravel/database/migrations/2016_05_24_071520_combine_user_company.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CombineUserCompany extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('combineusercompany', function (Blueprint $table) {
+            $table->string('company_id');
+            $table->string('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('combineusercompany', function (Blueprint $table) {
+            //
+        });
+    }
+}
+
